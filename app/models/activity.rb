245f09094d1ce2455d98d967370237ba02aedc56
ktmouk <ktmouk@gmail.com>
2019-08-20 20:59:04 +0900

    validateのmethodをprivateに変更

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -30,20 +30,6 @@ class Activity < ApplicationRecord
     deliver_webhooks 'stopped'
   end
 
-  def project_is_invalid
-    return if project_id.nil?
-    return if user.projects.exists?(id: project_id)
-
-    errors.add(:project, :is_invalid)
-  end
-
-  def stopped_at_cannot_be_before_started_at
-    return if started_at.nil? || stopped_at.nil?
-    return if stopped_at >= started_at
-
-    errors.add(:stopped_at, :cannot_be_before_started_at)
-  end
-
   def set_duration
     self.duration = stopped_at.present? ? stopped_at - started_at : nil
   end
@@ -55,5 +41,19 @@ class Activity < ApplicationRecord
 
   def self.working
     find_by(stopped_at: nil)
+  end
+
+  private
+
+  def project_is_invalid
+    return if project_id.nil? || user.projects.exists?(id: project_id)
+
+    errors.add(:project, :is_invalid)
+  end
+
+  def stopped_at_cannot_be_before_started_at
+    return if started_at.nil? || stopped_at.nil? || stopped_at >= started_at
+
+    errors.add(:stopped_at, :cannot_be_before_started_at)
   end
 end
