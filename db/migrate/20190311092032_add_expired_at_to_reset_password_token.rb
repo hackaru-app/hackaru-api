977a17a1d59a2cb91606a2a35317fac1d19c4e99
ktmouk <ktmouk@gmail.com>
2019-08-21 12:44:47 +0900

    migrationを削除

--- db/migrate/20190311092032_add_expired_at_to_reset_password_token.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddExpiredAtToResetPasswordToken < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reset_password_tokens, :expired_at, :datetime, null: false
-  end
-end
--- db/migrate/20190312020257_rename_reset_password_tokens_to_password_reset_tokens.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RenameResetPasswordTokensToPasswordResetTokens <
-    ActiveRecord::Migration[5.2]
-  def change
-    rename_table :reset_password_tokens, :password_reset_tokens
-  end
-end
