b2aa46956719b28151a4a8830ca5d85cc08d1a07
ktmouk <ktmouk@gmail.com>
2019-08-24 00:34:07 +0900

    Faker::Date.betweenの引数をキーワード引数に修正

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     user
     project { create(:project, user: user) }
     description { Faker::Job.title }
-    started_at { Faker::Date.between(7.days.ago, 4.days.ago) }
-    stopped_at { Faker::Date.between(3.days.ago, 1.days.ago) }
+    started_at { Faker::Date.between(from: 7.days.ago, to: 4.days.ago) }
+    stopped_at { Faker::Date.between(from: 3.days.ago, to: 1.days.ago) }
   end
 end
