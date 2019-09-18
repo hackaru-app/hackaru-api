379aed1f30bec674aad5874f1f161f9be1e85924
ktmouk <ktmouk@gmail.com>
2019-09-19 00:17:25 +0900

    画像のディレクトリ位置を変更

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -8,7 +8,7 @@ module EmailHelper
   private
 
   def email_image_url(image)
-    attachments[image] = File.read Rails.root.join("app/assets/images/#{image}")
+    attachments[image] = File.read Rails.root.join("public/images/#{image}")
     attachments[image].url
   end
 end
