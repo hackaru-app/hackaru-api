b3ec28182a24dd58ab6b7bb8ebbbd5d67a2df89b
ktmouk <ktmouk@gmail.com>
2019-09-23 00:20:41 +0900

    utm_urlに変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  track message: false
-  track utm_params: true
-
   helper :email
 
   default from: ENV.fetch('SMTP_FROM', 'no-reply@example.com')
