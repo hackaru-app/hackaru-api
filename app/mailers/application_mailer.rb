dd024f0616c0dfec4fd6e72ff516990d5c3104aa
ktmouk <ktmouk@gmail.com>
2019-09-22 22:28:31 +0900

    ahoy_emailを追加

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  track utm_params: true
+
   helper :email
 
   default from: ENV.fetch('SMTP_FROM', 'no-reply@example.com')
