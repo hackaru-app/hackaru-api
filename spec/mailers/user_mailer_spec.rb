c9d185f211e1656cc2db25f33ce929044ba322cb
ktmouk <ktmouk@gmail.com>
2019-09-25 23:48:55 +0900

    testを修正

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -11,21 +11,19 @@ RSpec.describe UserMailer, type: :mailer do
       expect(subject.to.first).to eq(user.email)
     end
 
-    describe 'html' do
-      it 'has password-reset url in body' do
-        path = "#{ENV.fetch('HACKARU_WEB_URL')}/password-reset/edit"
-        query = "token=.+&amp;user_id=#{user.id}"
-        body = subject.parts[0].body.parts[1].body.raw_source
-        expect(body).to match(/#{path}\?#{query}/)
-      end
-    end
-
     describe 'text' do
       it 'has password-reset url in body' do
         path = "#{ENV.fetch('HACKARU_WEB_URL')}/password-reset/edit"
         query = "token=.+&user_id=#{user.id}"
-        body = subject.parts[0].body.parts[0].body
-        expect(body).to match(/#{path}\?#{query}/)
+        expect(subject.parts[0].body).to match(/#{path}\?#{query}/)
+      end
+    end
+
+    describe 'html' do
+      it 'has password-reset url in body' do
+        path = "#{ENV.fetch('HACKARU_WEB_URL')}/password-reset/edit"
+        query = "token=.+&amp;user_id=#{user.id}"
+        expect(subject.parts[1].body.raw_source).to match(/#{path}\?#{query}/)
       end
     end
   end
