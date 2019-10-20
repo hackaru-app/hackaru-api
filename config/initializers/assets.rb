9399efa27b1ef210de42dabc86fa77f1c0ed2f4d
ktmouk <ktmouk@gmail.com>
2019-10-20 17:58:51 +0900

    rubocopのエラーを修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w[c3/c3.min.css c3/c3.min.js d3/dist/d3.min.js mailer.scss pdf.scss]
+Rails.application.config.assets.precompile += %w[
+  c3/c3.min.css
+  c3/c3.min.js
+  d3/dist/d3.min.js
+  mailer.scss
+  pdf.scss
+]
