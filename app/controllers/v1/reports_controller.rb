4402123a8092963f95a3d83897cb932ed1d972b1
ktmouk <ktmouk@gmail.com>
2019-10-22 15:25:00 +0900

    projectの表示を追加

--- app/controllers/v1/reports_controller.rb
+++ app/controllers/v1/reports_controller.rb
@@ -6,6 +6,13 @@ module V1
     include ActionController::MimeResponds
 
     def show
+      param! :start, Time, required: true
+      param! :end, Time, required: true
+
+      @start = params[:start]
+      @end = params[:end]
+      @projects = User.find(1).projects # TODO
+
       respond_to do |format|
         format.html { render :show, formats: [:html] }
         format.pdf { render_pdf :show }
