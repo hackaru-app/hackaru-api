0d18102902b0eb8f9fd46d342d028170b206dbba
ktmouk <ktmouk@gmail.com>
2019-10-22 14:42:53 +0900

    normalize.cssを追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,10 +2,11 @@
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w[
+  normalize.css/normalize.css
+  mailer.scss
+  pdf.scss
   c3/c3.min.css
   c3/c3.min.js
   d3/dist/d3.min.js
   report.js
-  mailer.scss
-  pdf.scss
 ]
