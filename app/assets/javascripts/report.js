9f667482f7600c70085199c23795063fc9b4e9da
ktmouk <ktmouk@gmail.com>
2019-10-22 13:32:24 +0900

    reportのjsを追加

---
+++ app/assets/javascripts/report.js
@@ -0,0 +1,10 @@
+c3.generate({
+  bindto: '#pie-chart',
+  data: {
+    type : 'pie',
+    columns: [
+        ['data1', 30],
+        ['data2', 120],
+    ],
+  }
+});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.precompile += %w[
   c3/c3.min.css
   c3/c3.min.js
   d3/dist/d3.min.js
+  report.js
   mailer.scss
   pdf.scss
 ]
