147d921a889194d1ccb43f77de1047c5af43abca
ktmouk <ktmouk@gmail.com>
2019-10-22 13:40:19 +0900

    donut-chartに変更

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -1,10 +1,19 @@
 c3.generate({
-  bindto: '#pie-chart',
+  bindto: '#donut-chart',
   data: {
-    type : 'pie',
+    type : 'donut',
     columns: [
         ['data1', 30],
         ['data2', 120],
-    ],
+    ]
+  },
+  donut: {
+    padAngle: .015,
+    label: {
+      show: false
+    }
+  },
+  legend: {
+    show: false
   }
 });
