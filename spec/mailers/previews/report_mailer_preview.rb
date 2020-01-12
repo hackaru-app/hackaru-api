f08809bd447ada06fbf7296924c2753d6c332148
ktmouk <ktmouk@gmail.com>
2020-01-12 22:39:20 +0900

    previewを修正

--- spec/mailers/previews/report_mailer_preview.rb
+++ spec/mailers/previews/report_mailer_preview.rb
@@ -5,25 +5,25 @@ class ReportMailerPreview < ActionMailer::Preview
   def weekly
     title = I18n.t('jobs.report_mailer_job.week.title')
     range = Date.today.prev_week.all_week
-    report(title, range)
+    report(title, range.begin, range.end)
   end
 
   def monthly
     title = I18n.t('jobs.report_mailer_job.month.title')
     range = Date.today.prev_month.all_month
-    report(title, range)
+    report(title, range.begin, range.end)
   end
 
   private
 
-  def report(title, range)
+  def report(title, from, to)
     user = FactoryBot.create(:user)
     FactoryBot.create_list(
       :activity, 3,
       user: user,
-      started_at: range.begin,
-      stopped_at: range.begin + 1.hours
+      started_at: from,
+      stopped_at: to + 1.hours
     )
-    ReportMailer.report(user, title, range)
+    ReportMailer.report(user, title, from, to)
   end
 end
