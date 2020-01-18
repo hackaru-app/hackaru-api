63ee1deb7a16f8eeba473b629e40552ac9c62b74
ktmouk <ktmouk@gmail.com>
2020-01-18 16:34:28 +0900

    schema.rbを保存

--- db/schema.rb
+++ db/schema.rb
@@ -1,134 +1,143 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_125429) do
-
+ActiveRecord::Schema.define(version: 20_200_118_053_142) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "activities", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "project_id"
-    t.string "description", default: "", null: false
-    t.datetime "started_at", null: false
-    t.datetime "stopped_at"
-    t.integer "duration"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_activities_on_project_id"
-    t.index ["user_id"], name: "index_activities_on_user_id"
+  create_table 'activities', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'project_id'
+    t.string 'description', default: '', null: false
+    t.datetime 'started_at', null: false
+    t.datetime 'stopped_at'
+    t.integer 'duration'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['project_id'], name: 'index_activities_on_project_id'
+    t.index ['user_id'], name: 'index_activities_on_user_id'
   end
 
-  create_table "oauth_access_grants", force: :cascade do |t|
-    t.bigint "resource_owner_id", null: false
-    t.bigint "application_id", null: false
-    t.string "token", null: false
-    t.integer "expires_in", null: false
-    t.text "redirect_uri", null: false
-    t.datetime "created_at", null: false
-    t.datetime "revoked_at"
-    t.string "scopes"
-    t.index ["application_id"], name: "index_oauth_access_grants_on_application_id"
-    t.index ["resource_owner_id"], name: "index_oauth_access_grants_on_resource_owner_id"
-    t.index ["token"], name: "index_oauth_access_grants_on_token", unique: true
+  create_table 'activity_calendars', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'token'
+    t.index ['token'], name: 'index_activity_calendars_on_token', unique: true
+    t.index ['user_id'], name: 'index_activity_calendars_on_user_id', unique: true
   end
 
-  create_table "oauth_access_tokens", force: :cascade do |t|
-    t.bigint "resource_owner_id"
-    t.bigint "application_id"
-    t.string "token", null: false
-    t.string "refresh_token"
-    t.integer "expires_in"
-    t.datetime "revoked_at"
-    t.datetime "created_at", null: false
-    t.string "scopes"
-    t.string "previous_refresh_token", default: "", null: false
-    t.index ["application_id"], name: "index_oauth_access_tokens_on_application_id"
-    t.index ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
-    t.index ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
-    t.index ["token"], name: "index_oauth_access_tokens_on_token", unique: true
+  create_table 'oauth_access_grants', force: :cascade do |t|
+    t.bigint 'resource_owner_id', null: false
+    t.bigint 'application_id', null: false
+    t.string 'token', null: false
+    t.integer 'expires_in', null: false
+    t.text 'redirect_uri', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'revoked_at'
+    t.string 'scopes'
+    t.index ['application_id'], name: 'index_oauth_access_grants_on_application_id'
+    t.index ['resource_owner_id'], name: 'index_oauth_access_grants_on_resource_owner_id'
+    t.index ['token'], name: 'index_oauth_access_grants_on_token', unique: true
   end
 
-  create_table "oauth_applications", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "uid", null: false
-    t.string "secret", null: false
-    t.text "redirect_uri", null: false
-    t.string "scopes", default: "", null: false
-    t.boolean "confidential", default: true, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "owner_id"
-    t.string "owner_type"
-    t.index ["owner_id", "owner_type"], name: "index_oauth_applications_on_owner_id_and_owner_type"
-    t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
+  create_table 'oauth_access_tokens', force: :cascade do |t|
+    t.bigint 'resource_owner_id'
+    t.bigint 'application_id'
+    t.string 'token', null: false
+    t.string 'refresh_token'
+    t.integer 'expires_in'
+    t.datetime 'revoked_at'
+    t.datetime 'created_at', null: false
+    t.string 'scopes'
+    t.string 'previous_refresh_token', default: '', null: false
+    t.index ['application_id'], name: 'index_oauth_access_tokens_on_application_id'
+    t.index ['refresh_token'], name: 'index_oauth_access_tokens_on_refresh_token', unique: true
+    t.index ['resource_owner_id'], name: 'index_oauth_access_tokens_on_resource_owner_id'
+    t.index ['token'], name: 'index_oauth_access_tokens_on_token', unique: true
   end
 
-  create_table "password_reset_tokens", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "expired_at", null: false
-    t.index ["token"], name: "index_reset_password_tokens_on_token", unique: true
-    t.index ["user_id"], name: "index_reset_password_tokens_on_user_id", unique: true
+  create_table 'oauth_applications', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'uid', null: false
+    t.string 'secret', null: false
+    t.text 'redirect_uri', null: false
+    t.string 'scopes', default: '', null: false
+    t.boolean 'confidential', default: true, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'owner_id'
+    t.string 'owner_type'
+    t.index %w[owner_id owner_type], name: 'index_oauth_applications_on_owner_id_and_owner_type'
+    t.index ['uid'], name: 'index_oauth_applications_on_uid', unique: true
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.string "color"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id", "name"], name: "index_projects_on_user_id_and_name", unique: true
-    t.index ["user_id"], name: "index_projects_on_user_id"
+  create_table 'password_reset_tokens', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'token', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.datetime 'expired_at', null: false
+    t.index ['token'], name: 'index_password_reset_tokens_on_token', unique: true
+    t.index ['user_id'], name: 'index_password_reset_tokens_on_user_id', unique: true
   end
 
-  create_table "refresh_tokens", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "client_id", null: false
-    t.datetime "revoked_at"
-    t.index ["client_id"], name: "index_refresh_tokens_on_client_id", unique: true
-    t.index ["token"], name: "index_refresh_tokens_on_token", unique: true
-    t.index ["user_id"], name: "index_refresh_tokens_on_user_id"
+  create_table 'projects', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'name', null: false
+    t.string 'color'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[user_id name], name: 'index_projects_on_user_id_and_name', unique: true
+    t.index ['user_id'], name: 'index_projects_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "password_digest", default: "", null: false
-    t.string "email", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+  create_table 'refresh_tokens', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'token', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'client_id', null: false
+    t.datetime 'revoked_at'
+    t.index ['client_id'], name: 'index_refresh_tokens_on_client_id', unique: true
+    t.index ['token'], name: 'index_refresh_tokens_on_token', unique: true
+    t.index ['user_id'], name: 'index_refresh_tokens_on_user_id'
   end
 
-  create_table "webhooks", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "target_url", null: false
-    t.string "event", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id", "target_url", "event"], name: "index_webhooks_on_user_id_and_target_url_and_event", unique: true
-    t.index ["user_id"], name: "index_webhooks_on_user_id"
+  create_table 'users', force: :cascade do |t|
+    t.string 'password_digest', default: '', null: false
+    t.string 'email', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
-  add_foreign_key "activities", "projects", on_delete: :nullify
-  add_foreign_key "activities", "users", on_delete: :cascade
-  add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
-  add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
-  add_foreign_key "password_reset_tokens", "users", on_delete: :cascade
-  add_foreign_key "projects", "users", on_delete: :cascade
-  add_foreign_key "refresh_tokens", "users", on_delete: :cascade
-  add_foreign_key "webhooks", "users", on_delete: :cascade
+  create_table 'webhooks', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'target_url', null: false
+    t.string 'event', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[user_id target_url event], name: 'index_webhooks_on_user_id_and_target_url_and_event', unique: true
+    t.index ['user_id'], name: 'index_webhooks_on_user_id'
+  end
+
+  add_foreign_key 'activities', 'projects', on_delete: :nullify
+  add_foreign_key 'activities', 'users', on_delete: :cascade
+  add_foreign_key 'activity_calendars', 'users', on_delete: :cascade
+  add_foreign_key 'oauth_access_grants', 'oauth_applications', column: 'application_id'
+  add_foreign_key 'oauth_access_tokens', 'oauth_applications', column: 'application_id'
+  add_foreign_key 'password_reset_tokens', 'users', on_delete: :cascade
+  add_foreign_key 'projects', 'users', on_delete: :cascade
+  add_foreign_key 'refresh_tokens', 'users', on_delete: :cascade
+  add_foreign_key 'webhooks', 'users', on_delete: :cascade
 end
