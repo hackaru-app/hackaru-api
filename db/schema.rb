1319316b88e224d949456f1a40d0246296580cb5
ktmouk <ktmouk@gmail.com>
2020-01-20 19:59:12 +0900

    time_zoneのcolumnを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_084353) do
+ActiveRecord::Schema.define(version: 2020_01_20_105107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2020_01_18_084353) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "receive_week_report", default: true, null: false
     t.boolean "receive_month_report", default: true, null: false
+    t.string "time_zone", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
