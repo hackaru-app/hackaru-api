fd18d2a76a27ff7898f3bf14cc6ff19a76bc98ac
ktmouk <ktmouk@gmail.com>
2020-02-08 02:18:25 +0900

    localeのcolumnを追加

---
+++ db/migrate/20200207171321_add_locale_column_to_users.rb
@@ -0,0 +1,6 @@
+class AddLocaleColumnToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :locale, :string, null: true
+    change_column_null :users, :locale, false, 'ja'
+  end
+end
