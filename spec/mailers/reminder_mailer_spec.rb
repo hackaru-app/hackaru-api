69d7df5d5e48ae81202181285a74183ac31d92ad
sainu <saino.katsutoshi@moneyforward.co.jp>
2020-02-22 00:20:30 +0900

    reminderで受け取るactivityを単一のインスタンスになるように変更

--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -5,10 +5,10 @@ require 'rails_helper'
 RSpec.describe ReminderMailer, type: :mailer do
   describe '#report' do
     let(:user) { create(:user) }
-    let(:activities) { create_list(:activity, 2, user: user) }
+    let(:activity) { create(:activity, user: user) }
 
     subject do
-      ReminderMailer.remind(user, activities)
+      ReminderMailer.remind(user, activity)
     end
 
     it 'send to user' do
