419137200da0b4b85061846d948363b6d84ab7fe
sainu <saino.katsutoshi@moneyforward.co.jp>
2020-02-24 22:10:59 +0900

    ReminderMailerが通知するアクティビティにはdurationが算出できないのでviewからdurationの表示を削除

--- spec/mailers/previews/reminder_mailer_preview.rb
+++ spec/mailers/previews/reminder_mailer_preview.rb
@@ -3,7 +3,11 @@
 # Preview all emails at http://localhost:3000/rails/mailers/reminder_mailer
 class ReminderMailerPreview < ActionMailer::Preview
   def remind
-    activity = FactoryBot.create(:activity)
+    activity = FactoryBot.create(
+      :activity,
+      started_at: 5.hours.ago,
+      stopped_at: nil
+    )
     ReminderMailer.remind(activity)
   end
 end
