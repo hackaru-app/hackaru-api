88948596524672ba6143524ba8163c4e048e7a2f
ktmouk <ktmouk@gmail.com>
2021-02-02 11:26:55 +0900

    Remove email helper

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  helper :email
   helper :duration
 
   default from: "Hackaru <#{ENV.fetch('SMTP_FROM', 'no-reply@example.com')}>"
