483981bc91338fa7ba9bdd31786d4eeeda249f54
ktmouk <ktmouk@gmail.com>
2021-02-07 23:32:22 +0900

    Fixed rubocop erorr

--- spec/support/authenticate_helper.rb
+++ spec/support/authenticate_helper.rb
@@ -17,40 +17,6 @@ module AuthenticateHelper
     cookie_jar.signed[key] = value
     cookies[key] = cookie_jar[key]
   end
-
-  def login(user = create(:user))
-    return unless user
-
-    auth_token = create(:auth_token, user: user, token: 'secret')
-    store_signed_cookie(:auth_token_id, auth_token.id)
-    store_signed_cookie(:auth_token_raw, 'secret')
-  end
-
-  private
-
-  def store_signed_cookie(key, value)
-    env_config = Rails.application.env_config.deep_dup
-    cookie_jar = ActionDispatch::Request.new(env_config).cookie_jar
-    cookie_jar.signed[key] = value
-    cookies[key] = cookie_jar[key]
-  end
-
-  def login(user = create(:user))
-    return unless user
-
-    auth_token = create(:auth_token, user: user, token: 'secret')
-    store_signed_cookie(:auth_token_id, auth_token.id)
-    store_signed_cookie(:auth_token_raw, 'secret')
-  end
-
-  private
-
-  def store_signed_cookie(key, value)
-    env_config = Rails.application.env_config.deep_dup
-    cookie_jar = ActionDispatch::Request.new(env_config).cookie_jar
-    cookie_jar.signed[key] = value
-    cookies[key] = cookie_jar[key]
-  end
 end
 
 RSpec.configure do |config|
