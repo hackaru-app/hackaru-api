f50231ebc8c318878905b61ae64e5e42662a831e
ktmouk <ktmouk@gmail.com>
2021-08-02 21:50:07 +0900

    Update log format to short

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 if ENV['AWS_SES_REGION'] && ENV['AWS_SES_ACCESS_KEY_ID'] && ENV['AWS_SES_SECRET_ACCESS_KEY']
+  Aws.config.update(log_formatter: Aws::Log::Formatter.short)
   Aws::Rails.add_action_mailer_delivery_method(
     :ses,
     region: ENV.fetch('AWS_SES_REGION'),
