92f8d5ac00662c5f949613db38fbe2e90dd7736d
ktmouk <ktmouk@gmail.com>
2021-12-19 19:49:14 +0900

    Add SENTRY_ENVIRONMENT

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,7 @@
 
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
+  config.environment = ENV['SENTRY_ENVIRONMENT']
   config.breadcrumbs_logger = [:active_support_logger]
   config.traces_sample_rate = 0.5
 
