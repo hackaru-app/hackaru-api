86c43db708e340927d9204b6f94e54d3239aee7f
Zhiyuan Zheng <zhzy0077@hotmail.com>
2022-10-17 19:52:09 +0800

    add zh-CN

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module HackaruApi
 
     config.action_mailer.preview_path = Rails.root.join('spec/mailers/previews')
 
-    config.i18n.available_locales = %i[ja en]
+    config.i18n.available_locales = %i[ja en zh-CN]
     config.i18n.load_path += Dir[
       Rails.root.join('config/locales/**/*.{rb,yml}')
     ]
